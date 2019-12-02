d1a7278b7f997a946900c1a8ea6160ba8743b1a4
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2019-12-02 22:34:41 +0000

    Initial commit

---
+++ website_blocker.py
@@ -0,0 +1,21 @@
+import time
+from datetime import datetime as dt
+hosts_temp = "hosts"
+hosts_path = "C:\\Windows\\System32\\drivers\\etc\\hosts"
+redirect = "127.0.0.1"
+website_list = ["www.reddit.com", "reddit.com", "www.rte.ie", "rte.ie"]
+
+while True:
+    if dt(dt.now().year, dt.now().month, dt.now().day, 21) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 23):        
+        print("working hours")
+        with open(hosts_temp, 'r+') as file:
+            content = file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect + " " + website + "\n")
+    else:
+        print("non working hours")
+    time.sleep(5)
+        
